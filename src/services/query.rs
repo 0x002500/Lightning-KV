ded462c239555a95c3cc2770c086698f19ca1214
0x002500 <tiger_pan@outlook.com>
2024-07-11 18:12:11 +0800

    [fix] Fixed the issus caused by merging

--- src/services/query.rs
+++ src/services/query.rs
@@ -10,13 +10,9 @@ pub async fn index(path: web::types::Path<String>) -> Result<String, web::Error>
     if let Some(value) = GLOBAL_MAP.get(key) {
         return Ok(value.value().clone());
     }
-
     Err(web::error::ErrorBadRequest("requested key not found").into())
 
-    let query: Option<dashmap::mapref::one::Ref<String, String>>  = GLOBAL_MAP.get(key);
-    let rep: String = query.unwrap().to_string();
     //let duration = start.elapsed();
 
     //println!("Time elapsed in query() is: {:?}", duration);
-    Ok(rep)
 }
