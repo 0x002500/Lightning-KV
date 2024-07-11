033905ac611ab02246cc71d35f0b53e2782d5885
0x002500 <tiger_pan@outlook.com>
2024-07-11 17:18:52 +0800

    [feat] Add lines to test the time taken by the functions

--- src/services/query.rs
+++ src/services/query.rs
@@ -1,9 +1,14 @@
 use crate::GLOBAL_MAP;
 use ntex::web;
+//use std::time::{Instant, Duration};
 
 pub async fn index(path: web::types::Path<String>) -> Result<String, web::Error> {
+    //let start = Instant::now();
     let key: &String = &path.into_inner();
     let query: Option<dashmap::mapref::one::Ref<String, String>>  = GLOBAL_MAP.get(key);
     let rep: String = query.unwrap().to_string();
+    //let duration = start.elapsed();
+
+    //println!("Time elapsed in query() is: {:?}", duration);
     Ok(rep)
 }
