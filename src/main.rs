d39829150e1d0b5c7fa6fb050af970d795c856ce
0x002500 <tiger_pan@outlook.com>
2024-07-11 17:06:44 +0800

    [feat] Use DashMap to store the data

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use dashmap::DashMap;
+use lazy_static::lazy_static;
 use ntex::web;
+use std::sync::Arc;
 
 mod services;
+
+lazy_static! {
+    pub static ref GLOBAL_MAP: Arc<DashMap<String, String>> = Arc::new(DashMap::new());
+}
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
@@ -8,7 +15,7 @@ async fn main() -> std::io::Result<()> {
         web::App::new()
             .route("/", web::get().to(services::index::index))
             .route("/newput", web::post().to(services::newput::index))
-            .route("/query/{filename}", web::get().to(services::query::index))
+            .route("/query/{key}", web::get().to(services::query::index))
     })
     .bind(("127.0.0.1", 8080))?
     .workers(4)
--- src/services/query.rs
+++ src/services/query.rs
@@ -1,9 +1,9 @@
+use crate::GLOBAL_MAP;
 use ntex::web;
-use ntex_files::NamedFile;
-use std::path::PathBuf;
 
-
-pub async fn index(req: web::HttpRequest) -> Result<NamedFile, web::Error> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
-    Ok(NamedFile::open(path)?)
+pub async fn index(path: web::types::Path<String>) -> Result<String, web::Error> {
+    let key: &String = &path.into_inner();
+    let query: Option<dashmap::mapref::one::Ref<String, String>>  = GLOBAL_MAP.get(key);
+    let rep: String = query.unwrap().to_string();
+    Ok(rep)
 }
