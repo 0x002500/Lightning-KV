ba67fa31821b521a0134b45af418002726547d3b
0x002500 <tiger_pan@outlook.com>
2025-01-15 12:10:32 +0800

    feat: add query.rs for /query router

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use actix_web::{web, App, HttpServer};
 use services::create::create_interface;
+use services::query::query_interface;
 
 mod services;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/create", web::post().to(create_interface)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    println!("Server started at 127.0.0.1:8080");
+    HttpServer::new(|| {
+        App::new()
+            .route("/create", web::post().to(create_interface))
+            .route("/query", web::get().to(query_interface))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
---
+++ src/services/query.rs
@@ -0,0 +1,14 @@
+use actix_web::web;
+use serde::Deserialize;
+use actix_files::NamedFile;
+use std::path::PathBuf;
+
+#[derive(Deserialize)]
+pub struct QueryData {
+    key: String
+}
+
+pub async fn query_interface(data: web::Json<QueryData>) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = PathBuf::from(data.key.clone());
+    Ok(NamedFile::open(path)?)
+}
